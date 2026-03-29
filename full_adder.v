397de7a9badedea6274193614f12bba4c12c3c4c
snigdhabhardwaj <89563115+snigdhabhardwaj@users.noreply.github.com>
2026-03-30 04:30:22 +0530

    Add files via upload

---
+++ full_adder.v
@@ -0,0 +1,11 @@
+// Full Adder RTL Design
+module full_adder (
+    input  a,
+    input  b,
+    input  cin,
+    output sum,
+    output cout
+);
+    assign sum  = a ^ b ^ cin;
+    assign cout = (a & b) | (b & cin) | (a & cin);
+endmodule
